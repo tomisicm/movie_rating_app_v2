50059beca46cd92b8ea086ab026b6f8a3d7bfd9a
Mihailo Tomisic <mihailotomisic@MacBook-Pro-6.local>
2019-04-15 07:24:54 +0200

    socketio and vue socketio client

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 import './plugins/vuetify'
 
 import Vue from 'vue'
+
+import socketio from 'socket.io-client'
+import VueSocketIO from 'vue-socket.io'
+
 import store from './utils/store'
 import router from './router'
 
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+const SocketInstance = socketio.connect('http://localhost:3000', {
+  query: {
+    token: window.localStorage.getItem('token')
+  }
+})
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: SocketInstance
+}))
 
 export const eventBus = new Vue()
 
